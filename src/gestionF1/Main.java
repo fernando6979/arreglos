dd4612999e592b6f1f40b4f6b1d58e6065fbadec
aymann78 <aymanassidah78@gmail.com>
2024-04-02 10:14:21 +0200

    Hacemos que el main ejecute metodos de otras clases y cree objetos.

--- src/gestionF1/Main.java
+++ src/gestionF1/Main.java
@@ -11,5 +11,22 @@ public class Main {
      * @param args Los argumentos de la linea de comandos.
      */
     public static void main(String[] args) {
+        // Crear equipos
+        Equipo redBull = new Equipo("Red Bull", "Honda");
+        Equipo mercedes = new Equipo("Mercedes", "Mercedes");
+        // Agregar pilotos a los equipos
+        redBull.agregarPiloto(new Piloto("Max Verstappen", 33, "Países Bajos"));
+        redBull.agregarPiloto(new Piloto("Sergio Perez", 11, "México"));
+        mercedes.agregarPiloto(new Piloto("Lewis Hamilton", 44, "Reino Unido"));
+        mercedes.agregarPiloto(new Piloto("George Russell", 63, "Reino Unido"));
+        // Imprimir información de los equipos y pilotos
+        System.out.println("Equipo: " + redBull.getNombre());
+        for (Piloto piloto : redBull.getPilotos()) {
+            System.out.println(" - Piloto: " + piloto.getNombre() + ", Número de carrera: " + piloto.getNumeroCarrera() + ", País: " + piloto.getPais());
+        }
+        System.out.println("Equipo: " + mercedes.getNombre());
+        for (Piloto piloto : mercedes.getPilotos()) {
+            System.out.println(" - Piloto: " + piloto.getNombre() + ", Número de carrera: " + piloto.getNumeroCarrera() + ", País: " + piloto.getPais());
+        }
     }
 }
